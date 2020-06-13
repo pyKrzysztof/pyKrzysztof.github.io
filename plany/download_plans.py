e340e676ced8471b0371b18eda49ae6918d75a0f
pyKrzysztof <krzysztofdotpy@gmail.com>
2020-06-13 09:40:01 +0200

    restoring site

--- plany/download_plans.py
+++
@@ -1,7 +0,0 @@
-from urllib.request import urlretrieve
-
-addr = "http://plan.zs9elektronik.pl/plany/o.html"
-n = 31
-
-for i in range(1, n+1):
-    urlretrieve(addr.replace(".html", f"{i}.html"), f"o{i}.html")
--- plany/plan.js
+++
@@ -1,8 +0,0 @@
-function drukuj(){
-	if(self.parent.frames.length<2)
-		alert('Polecenie niedostêpne')
-	else{
-		self.parent.plan.focus()
-		window.print()
-	}
-}
